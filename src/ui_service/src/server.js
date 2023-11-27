f68fd17ec5fc05973a39c4e0be6d995c141e2196
Ashton Wilbern <aswi7205@colorado.edu>
2023-11-26 22:49:47 -0600

    Further work on frontend

--- src/ui_service/src/server.js
+++ src/ui_service/src/server.js
@@ -1,19 +1,53 @@
+// Import Dependencies
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+// App Settings
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views/"));
 
+// API Routes
 app.get("/", (req, res) => {
   res.render("pages/index");
+});
+
+app.get('/register', (req, res) => {
+  res.render('pages/register');
+});
+
+app.get('/login', (req, res) => {
+  res.render('pages/login');
+});
+
+app.get("/browse", (req, res) => {
+  res.render("pages/browse");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("pages/cart");
+});
+
+app.get("/account", (req, res) => {
+  res.render("pages/account");
 });
 
 app.get("*", (req, res) => {
   res.status(404).render("pages/error/404");
 });
 
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('pages/login', { message: 'Logged out Successfully' });
+    }
+  });
+});
+
+// Start Server
 app.listen(3000, () => {
   console.log("UI Server is running");
 });
