32c0d668ce30ebfbd4cc49879e58eda6f64e4757
leteg3n <112197235+leteg3n@users.noreply.github.com>
2023-12-04 23:57:17 -0700

    Add files via upload (#13)
    * Cart test files
    
    * Add files via upload
    
    * Database init strcturing
    
    * logs
    
    * cart page

--- src/cart_service/database/init.sql
+++ src/cart_service/database/init.sql
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
+--Database for a logged in consumer user to view items in their cart
+CREATE TABLE cart (
+    id PRIMARY KEY SERIAL,
+    user INT,
+    title VARCHAR(256),
+    description VARCHAR(1000),
+    price FLOAT NOT NULL,
+=======
 -- Table to store cart information
 CREATE TABLE cart (
     cart_id SERIAL PRIMARY KEY,
@@ -8,4 +17,5 @@ CREATE TABLE cart (
     created_at DATETIME,
     FOREIGN KEY (user_id) REFERENCES abstract_user (id),
     FOREIGN KEY (product_id) REFERENCES product (id)
+>>>>>>> 7cbd618d521d2b6b240bad49d1af4bfb2f8fc55b
 );
