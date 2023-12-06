a10e9f4d44b34c0fefebfacc772c3952f91c82af
leteg3n <lete1842@colorado.edu>
2023-12-05 21:52:20 -0700

    cart page and some routes

--- src/webserver/src/routes/cart.js
+++ src/webserver/src/routes/cart.js
@@ -13,6 +13,18 @@ router.route("/").get((req, res, next) => {
   });
 });
 
+router.route("/addToCart").post((req, res, next) => {
+  res.status(200).json({
+    message: "Item successfully added",
+  });
+});
+
+router.route("/removeFromCart").delete((req, res, next) => {
+  res.status(200).json({
+    message: "Item successfully removed",
+  });
+});
+
 // TODO: Add other routes related to cart (add/remove/etc.)
 
 module.exports = router;
