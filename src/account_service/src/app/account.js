8132c48f53a3077eb28230ad60e774d52bf26f0d
Owen Dyer <owenldyer@gmail.com>
2023-12-08 21:41:41 -0700

    Made some small changes to frontend files

--- src/account_service/src/app/account.js
+++ src/account_service/src/app/account.js
@@ -2,7 +2,7 @@ const db = require("../database/init");
 const jwt = require("jsonwebtoken");
 const { publicKey } = require("../crypto/load_key_pair");
 const { account } = require("../database/sql");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 // TODO: Implement authorization stuff so that this is secure
 // For just just gonna check the account id in the JWT cuz whatever
